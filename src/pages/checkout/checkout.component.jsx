7a608655d6eadd79d6b139b2eae802ea677e0b14
David <j626kim@uwaterloo.ca>
2020-02-25 03:32:01 -0500

    updating stripe integration, updating favicon and title

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -36,6 +36,11 @@ const Checkout = ({ cartItems, totalPrice }) => {
 			})}
 
 			<div className="total-price">${totalPrice}</div>
+			<div className="test-warning">
+				*Please use the following test credit card for payments*
+				<br />
+				4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+			</div>
 			<StripeButton price={totalPrice} />
 		</div>
 	);
